90a5327bce4df90e306b1a5c218bdf8845521efc
coreocto <coreocto@hotmail.com>
2018-02-17 00:51:59 +0800

    removed redundant class

--- src/main/java/Chlh1PgExt.java
+++ src/main/java/Chlh1PgExt.java
@@ -5,23 +5,25 @@ import org.postgresql.pljava.annotation.Function;
 import static org.postgresql.pljava.annotation.Function.Effects.IMMUTABLE;
 import static org.postgresql.pljava.annotation.Function.OnNullInput.RETURNS_NULL;
 
+import java.util.BitSet;
 import java.util.Base64;
 import java.util.Base64.Decoder;
-import java.util.BitSet;
 
-public class ChlhPgExt {
-	
+public class Chlh1PgExt {
 	@Function(onNullInput=RETURNS_NULL, effects=IMMUTABLE)
-	public static int match(String data, String query){
+	public static int Search(String dataBitSet, String queryBitSet) {
+		
 		Decoder b64Decoder = Base64.getDecoder();
-		BitSet dataBitSet = BitSet.valueOf(b64Decoder.decode(data));
-		BitSet queryBitSet = BitSet.valueOf(b64Decoder.decode(query));
-		dataBitSet.and(queryBitSet);
-		if (dataBitSet.equals(queryBitSet)){
+		
+		BitSet dBitSet = BitSet.valueOf(b64Decoder.decode(dataBitSet));
+		BitSet qBitSet = BitSet.valueOf(b64Decoder.decode(queryBitSet));
+		
+		dBitSet.and(qBitSet);
+		
+		if (dBitSet.equals(qBitSet)){
 			return 1;
 		}else{
 			return 0;
 		}
-	}
-	
+    }
 }
